7dff0675d89d9df097137870b95eb6e8cdcb008f
MeghlaB <meghlab964@gmail.com>
2025-04-22 23:41:00 +0600

    dashboard added

---
+++ src/Dashboard/Dashboard.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+import { Outlet } from 'react-router-dom';
+import AdminDashboard from '../Components/AdminDashboard';
+import UserDashboard from '../Components/UserDashboard';
+
+export default function Dashboard() {
+    const isAdmin = 'admin'
+  return (
+    <div>
+      <div>
+      <div className="flex">
+        {/* Sidebar */}
+        <div className="w-[250px] fixed h-full bg-gray-100">
+        {isAdmin ? <AdminDashboard/>:<UserDashboard/>}
+          {/* <SellerSidebar /> */}
+        </div>
+
+        {/* Main Content */}
+        <div className="md:ml-[250px] w-full">
+          
+          <main className="pt-6 px-6">
+
+          <Outlet/>
+          </main>
+        </div>
+      </div>
+    </div>
+    </div>
+  )
+}
--- src/Router/Route.jsx
+++ src/Router/Route.jsx
@@ -6,6 +6,10 @@ import Application from "../Pages/Application/Application";
 import Blog_Deatils from "../Components/Blog_Deatils";
 import Login from "../Pages/Login/Login";
 import Register from "../Pages/Register.jsx/Register";
+import Privetroutes from "./Privetroutes";
+import Dashboard from "../Dashboard/Dashboard";
+import AdminHome from "../Dashboard/Admin/AdminHome";
+
 
 const router = createBrowserRouter([
   {
@@ -38,6 +42,18 @@ const router = createBrowserRouter([
       },
     ],
   },
+  {
+    path:'/dashboard',
+    element:<Privetroutes><Dashboard/></Privetroutes>,
+    children:[
+      {
+        path:'',
+        
+        element:<AdminHome/>
+      },
+     
+    ]
+  }
 ]);
 
 export default router;
